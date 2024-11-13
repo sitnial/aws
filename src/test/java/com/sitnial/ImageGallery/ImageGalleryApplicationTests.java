0e2bbd26495cf1b27ca62ebb3905a5669468013b
AlexandraSitnikova <sitnikovalexandra00@gmail.com>
2024-11-13 22:30:50 +0100

    aws codebuild example

---
+++ src/test/java/com/sitnial/ImageGallery/ImageGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitnial.ImageGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
